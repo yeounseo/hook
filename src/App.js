c16303b145cb84fbf7d8b209d7dfe195f3794c5b
yeounseo <yeoandante@gmail.com>
2019-05-21 01:56:34 +0900

    Hook ! Study !

--- src/App.js
+++ src/App.js
@@ -1,26 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
+const useInput = initialValue => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: {value}
+    } = event;
+    setValue(value);
+  };
+  return { value, onChange };
+};
+
+const App = () => {
+  // const [item, setItem] = useState(1);
+  // const incrementItem = () => setItem(item + 1);
+  // const decrementItem = () => setItem(item - 1);
+  const name = useInput("Mr.");
+  const email = useInput("@");
   return (
+    // <div className="App">
+    //   <h1>Hello React Hook! {item}</h1>
+    //   <h2>Start editing to see some magic happen!</h2>
+    //   <button onClick={incrementItem}>incrementItem</button>
+    //   <button onClick={decrementItem}>decrementItem</button>
+    // </div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
+      <input placeholder="Email" {...email} />
     </div>
   );
+};
+
+class Ugly extends React.Component {
+  state = {
+    name: "",
+    email: ""
+  };
+  render() {
+    const { name, email } = this.state;
+    return (
+      <div className="App">
+        <input 
+          onChange={this.onChange}
+          placeholder="Name"
+          value={name}
+          name="name" 
+        />
+        <input 
+          onChange={this.onChange}
+          placeholder="Email" 
+          value={email} 
+          name="email" 
+        />
+      </div>
+    );
+  }
+  onChange = (event) => {
+    const {
+       target: { value, name }
+      } = event;
+    this.setState({
+      [name] : value
+    });
+  };
 }
+
+//  위의 코드는 hook을 이용한 코드 hook으로 만든다면 didmount , render 등을 하지 않아도 된다.
+// functional programming
+// 아래 코드는 hook이 없던때에 state를 이용한 코드
+
+// class App extends React.Component {
+//   state = {
+//     item: 1
+//   }
+  
+//   render() {
+//     const { item } = this.state;
+//     return (
+//       <div className="App">
+//         <h1>Hello React Hook! {item}</h1>
+//         <h2>Start editing to see some magic happen!</h2>
+//         <button onClick={this.incrementItem}>incrementItem</button>
+//         <button onClick={this.decrementItem}>decrementItem</button>
+//       </div>
+//     )
+//   }
+
+//   incrementItem = () => {
+//     this.setState(state => {
+//       return {
+//         item: state.item + 1
+//       };
+//     });
+//   };
+
+//   decrementItem = () => {
+//     this.setState(state => {
+//       return {
+//         item: state.item - 1
+//       };
+//     });
+//   };
+// }
 
 export default App;
